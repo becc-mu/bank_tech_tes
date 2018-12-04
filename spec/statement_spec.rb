8ba5d40e7e30366a6a72acf936dd689c3a07bcf4
becc-mu <becc.mulugeta@gmail.com>
2018-12-04 17:38:18 +0000

    cleaned debugging line

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,7 +1,6 @@
 require 'statement'
 
 describe Statement do
-  #let(:account) { double :account }
   it 'logs transaction history' do
     statement = Statement.new
     expect(statement.header).to eq(["date || credit || debit || balance"])
