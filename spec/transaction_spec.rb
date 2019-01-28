398fd6a7b9be375bcebdf614977ae262668f4132
becc-mu <becc.mulugeta@gmail.com>
2019-01-28 13:22:01 +0000

    transaction date

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -3,7 +3,8 @@ describe Transaction do
   let(:date) { double :date }
 
   it 'returns transaction items' do
-    allow(Time).to receive_message_chain(:now, :strftime).and_return(date)
+    allow(Time).to receive_message_chain(:now,
+      :strftime).and_return(date)
 
   end
 end
