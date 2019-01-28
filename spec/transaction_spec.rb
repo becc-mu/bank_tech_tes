2085b2a4ab99b40771b3c37a9560c1981dd642ca
becc-mu <becc.mulugeta@gmail.com>
2019-01-28 12:55:44 +0000

    extract transaction class

---
+++ spec/transaction_spec.rb
@@ -0,0 +1,9 @@
+require 'transaction'
+describe Transaction do
+  let(:date) { double :date }
+
+  it 'returns transaction items' do
+    allow(Time).to receive_message_chain(:now, :strftime).and_return(date)
+
+  end
+end
