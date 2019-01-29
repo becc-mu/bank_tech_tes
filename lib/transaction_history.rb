332daa7b250b744339fef34d5a3fcd8d9eeeacf4
becc-mu <becc.mulugeta@gmail.com>
2019-01-29 12:20:14 +0000

    passing transaction class

--- lib/transaction_history.rb
+++ lib/transaction_history.rb
@@ -1,2 +1,13 @@
 class TransactionHistory
-end 
+  attr_reader :transaction_history
+
+  def initialize(transaction = Transaction)
+    @transaction = transaction
+    @transaction_history = []
+  end
+
+  def log_transaction(deposit:, withdrawal:, balance:)
+    transaction = @transaction.new(deposit: deposit, withdrawal: withdrawal, balance: balance)
+    @transaction_history << transaction.transaction_item
+  end
+end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,10 +1,10 @@
 require 'transaction'
+
 describe Transaction do
   let(:date) { double :date }
-
   it 'returns transaction items' do
-    allow(Time).to receive_message_chain(:now,
-      :strftime).and_return(date)
-
+    allow(Time).to receive_message_chain(:now, :strftime).and_return(date)
+    transaction = Transaction.new(deposit: 100, withdrawal: nil, balance: 100)
+    expect(transaction.transaction_item).to eq(balance: 100, credit: 100, debit: nil, date: date)
   end
 end
